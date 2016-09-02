1f9c3e5b02479452023e71337bded5bc1c778085
Glyde <glydeb@yahoo.com>
2016-09-01 22:52:02 -0500

    connect view objects to controller

--- FoodTracker/FoodTracker/ViewController.swift
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -10,10 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: Properties
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
 
+    }
+    
+    // MARK: Properties
+
+    @IBOutlet weak var setDefaultLabelText: UIButton!
 }
 
