a50301025cc3c94e7a16580ba7aaf192b05f88ee
Glyde <glydeb@yahoo.com>
2016-09-06 22:34:14 -0500

    start build of custom control

---
+++ FoodTracker/FoodTracker/RatingControl.swift
@@ -0,0 +1,17 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Glydewell Burdick on 9/6/16.
+//  Copyright © 2016 Glydewell Burdick. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+
+    // MARK: Initialization
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
