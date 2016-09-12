615bb0588cc109ab71e4e6ceaf8ab8ad57f8817a
Glyde <glydeb@yahoo.com>
2016-09-11 22:20:48 -0500

    Add a loop to create 5 buttons instead of one

--- FoodTracker/FoodTracker/RatingControl.swift
+++ FoodTracker/FoodTracker/RatingControl.swift
@@ -9,15 +9,22 @@
 import UIKit
 
 class RatingControl: UIView {
+    // MARK: Properties
+    
+    var rating = 0
+    var ratingButtons = [UIButton]()
 
     // MARK: Initialization
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        
-        let button = UIButton (frame: CGRect(x: 0, y: 0, width: 44, height: 44))
-        button.backgroundColor = UIColor.redColor()
-        button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(_:)), forControlEvents: .TouchDown)
-        addSubview(button)
+      
+        for _ in 0..<5 {
+            let button = UIButton (frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+            button.backgroundColor = UIColor.redColor()
+            button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(_:)), forControlEvents: .TouchDown)
+            ratingButtons += [button]
+            addSubview(button)
+        }
       
     }
     
