c9650148cb5c2a31f58057ae51b4a5e798ff5006
Glyde <glydeb@yahoo.com>
2016-09-12 21:16:22 -0500

    Space buttons and add constants to replace hardcoded values

--- FoodTracker/FoodTracker/RatingControl.swift
+++ FoodTracker/FoodTracker/RatingControl.swift
@@ -13,15 +13,20 @@ class RatingControl: UIView {
     
     var rating = 0
     var ratingButtons = [UIButton]()
+    let spacing = 5
+    let starCount = 5
 
     // MARK: Initialization
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
       
-        for _ in 0..<5 {
+        for _ in 0..<starCount {
+            
             let button = UIButton (frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+            
             button.backgroundColor = UIColor.redColor()
             button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(_:)), forControlEvents: .TouchDown)
+            
             ratingButtons += [button]
             addSubview(button)
         }
@@ -31,6 +36,17 @@ class RatingControl: UIView {
     override func intrinsicContentSize() -> CGSize {
         return CGSize(width:240, height: 44)
     }
+    
+    override func layoutSubviews() {
+        var buttonFrame = CGRect(x: 0, y: 0, width: 44, height: 44)
+        
+        // Offset each button by the button's width plus spacing
+        for(index, button) in ratingButtons.enumerate() {
+            buttonFrame.origin.x = CGFloat(index * (44 + spacing))
+            button.frame = buttonFrame
+        }
+    }
+    
     // MARK: Button Action
     func ratingButtonTapped(button: UIButton) {
         print("button pressed 👍")
